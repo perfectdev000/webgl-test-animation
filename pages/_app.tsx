aacca175854368605d00f110f9353f4cd4b1a011
Zeus <84690417+darkrut22@users.noreply.github.com>
2022-06-01 01:51:13 +0900

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,5 @@
 import '../styles/globals.scss'
-import '../styles/loading.scss'
 import 'tailwindcss/tailwind.css'
-import '../styles/percent_circle.scss'
-import '../styles/item_card.scss'
-import '../styles/image_accordion.scss'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
